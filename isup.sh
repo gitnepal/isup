2d5c1ba29897f367065edf690844c28cd1c3dd01
Alpha <0x00hack3r@gmail.com>
2019-01-17 18:47:30 +0545

    Update isup.sh
    //Take from any working path 
    // Give error to invalid file
    //With counter

--- isup.sh
+++ isup.sh
@@ -8,6 +8,7 @@ RESET='\e[0m'
 TARGET="$1"
 CURRENT_PATH=$(pwd)
 
+
 if [ -z $TARGET ]; then
 echo -e "$RED  _                           _      $RESET "
 echo -e "$RED / |___ _   _ _ __        ___| |__   $RESET "
@@ -33,11 +34,18 @@ echo ""
 	echo -e "$GREEN [+] by @___0x00$RESET"
 	echo -e "$GREEN [+] http://rootnep.al$RESET"
     echo -e "$GREEN [+] Find alive host from huge domains dumps $RESET"
-	echo -e "$GREEN [-] Usage: isup.sh <targetlist, subdomain file>$RESET"
+	echo -e "$GREEN [-] Usage: isup.sh <files>$RESET"
+    exit
+fi
+
+if [ ! -f $TARGET ]; then
+    echo -e "$RED [+] ---------  File Not Found -------------- [+] $RESET"
+    echo -e "$RED [+] ---------  Check - FILE PATH -------------- [+] $RESET"
     exit
 fi
 
 createdir=$(mkdir -p "$CURRENT_PATH/tmp/")
+FILENAME=$( basename $TARGET )
 echo -e "$RED  _                           _      $RESET "
 echo -e "$RED / |___ _   _ _ __        ___| |__   $RESET "
 echo -e "$RED | / __| | | | '_ \      / __| '_ \  $RESET "
@@ -53,14 +61,18 @@ do
     ping -c1 -W1 $foo > /dev/null 2>&1
     if [[ $? -eq 0 ]];
     then
-        echo -e "$ORANGE [+]--- VALID ---[+] $foo $RESET" 
-        echo -e "$foo" | tee -a $CURRENT_PATH/tmp/valid-$TARGET > /dev/null 2>&1
-        
+        echo -e "$ORANGE [+]--- VALID ---[+] $foo $RESET"
+        echo -e "$foo" | tee -a $CURRENT_PATH/tmp/valid-$FILENAME > /dev/null 2>&1
+
     else
-        echo -e "$foo" | tee -a $CURRENT_PATH/tmp/notvalid-$TARGET
+        echo -e "$foo" | tee -a $CURRENT_PATH/tmp/notvalid-$FILENAME
     fi
 done
 echo -e ""
-echo -e "$BLUE  Working SubDomains saved to: tmp/valid-$TARGET.txt"
-echo -e "$BLUE  Invalid SubDomains saved to: tmp/notvalid-$TARGET.txt"
+echo -e "$BLUE  Working SubDomains saved to: tmp/valid-$FILENAME"
+echo -e "$BLUE  Invalid SubDomains saved to: tmp/notvalid-$FILENAME"
+vcounter=$(cat $CURRENT_PATH/tmp/valid-$FILENAME | sort -u | wc -l )
+fcounter=$(cat $CURRENT_PATH/tmp/notvalid-$FILENAME | sort -u | wc -l )
+orgcounter=$(cat $TARGET | sort -u | wc -l )
+echo -e "$RED   TOTAL DOMAINS : ${orgcounter} , ALIVE : ${vcounter} , DOWN : ${fcounter}  $RESET"
 echo -e "$BLUE + -- ----------------------------=[Done!]=----------------------------------- -- +$RESET"
